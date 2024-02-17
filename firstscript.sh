c0ee789bc9d623f17d16f981f47a508c134616ee
Santoshcs039 <144866454+Santoshcs039@users.noreply.github.com>
2024-02-17 17:32:45 +0530

    Created firstscript.sh from remote

---
+++ firstscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo " This is firstscript"
+echo " System uptime"
+uptime
+echo
+echo "Present working directory"
+pwd
+echo
+echo "listing all files and folders with timestamp"
+ls -lrt
+echo
